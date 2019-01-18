69f36b0faa1a7e9b316c5703649628e88dd6222b
Tyler <alexithemia@gmail.com>
2019-01-17 19:30:30 -1000

    Code for Item and tests for List created

--- js/shopping-list-item.js
+++ js/shopping-list-item.js
@@ -1,5 +1,29 @@
 class Item {
-
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.isDone = false;
+  }
+  check() {
+    this.isDone = true;
+  }
+  uncheck() {
+    this.isDone = false;
+  }
+  render() {
+    let start;
+    if (this.isDone === false) {
+      start = "<li class='completed_false'>";
+    } else if (this.isDone === true) {
+      start = "<li class='completed_true'>";
+    }
+    let end = '</li>';
+    let spanStart = '<span>';
+    let spanEnd = '</span>';
+    let listItem = spanStart + this.name + spanEnd;
+    listItem += spanStart + this.description + spanEnd;
+    return start + listItem + end;
+  }
 }
 
 module.exports = Item;
--- test/shopping-list.test.js
+++ test/shopping-list.test.js
@@ -5,75 +5,151 @@ let List = require('../js/shopping-list.js');
 
 describe("Shopping list Item", function () {
 
-  describe("shoppingListItem is a class", function () {
+  describe("Item is a class", function () {
 
     it("should be function", function () {
       expect(Item).to.be.a("function");
     })
   })
 
-  describe("has a property named name", function () {
-    let pizza = new Item('pizza', 'pepperoni pizza');
-    expect(pizza.name).to.equal('pizza');
-    expect(pizza.description).to.equal('pepperoni pizza');
-
-  })
-
-  describe("has a property named description", function () {
-    expect(Item).to.have.property('description');
-
-  })
-
-  describe("has a property named isDone", function () {
-    expect(Item).to.have.property('isDone');
-
+  describe("Item has a property", function () {
+    let steak = new Item('steak', 't-bone steak');
+    it("named 'name'", function () {
+      expect(steak).to.have.property('name');
+    })
+    it("named 'description'", function () {
+      expect(steak).to.have.property('description');
+    })
+    it('named "isDone"', function () {
+      expect(steak).to.have.property('isDone');
+    })
   })
 
   describe("has a method named check", function () {
-
+    let steak = new Item('steak', 't-bone steak');
     it("should be a function", function () {
-      expect(Item.check).to.be.a("function");
-
+      expect(steak.check).to.be.a("function");
     })
     it("should set items isDone property to true", function () {
-      let pasta = new Item('pasta', 'garlic cream fettucine');
-      pasta.check();
-      expect(pasta.isDone).to.be.true;
+      steak.check();
+      expect(steak.isDone).to.be.true;
     })
   })
 
   describe("has a method named uncheck", function () {
-
+    let steak = new Item('steak', 't-bone steak');
     it("should be a function", function () {
-      expect(Item.uncheck).to.be.a("function");
+      expect(steak.uncheck).to.be.a("function");
 
     })
     it("should set items isDone property to false", function () {
-      let salad = new Item('salad', 'caesar salad');
-      salad.check();
-      salad.uncheck();
-      expect(salad.isDone).to.be.false;
+      steak.check();
+      steak.uncheck();
+      expect(steak.isDone).to.be.false;
     })
 
   })
 
   describe("has a method named render", function () {
-
+    let steak = new Item('steak', 't-bone steak');
+    console.log(steak.render())
     it("should be a function", function () {
-      expect(Item.render).to.be.a("function");
-
+      expect(steak.render).to.be.a("function");
     })
 
     it("should return a string", function () {
-      let chicken = new Item('chicken', 'fried chicken');
-      expect(chicken.render()).to.be.a("string");
+      expect(steak.render()).to.be.a("string");
 
     })
 
-    it("should be wrapped in li tags", function () {
-      let seafood = new Item('seafood', 'crab legs');
-      expect(seafood.render().slice(0, 4)).to.equal('<li>');
-      expect(seafood.render().slice(0, -5)).to.equal('</li>');
+    it("should start with an <li tag", function () {
+      expect(steak.render().slice(0, 3)).to.equal('<li');
+
+    })
+    it("should end with an </li> tag", function () {
+      expect(steak.render().slice(-5)).to.equal('</li>');
+    })
+  })
+})
+
+describe('Shopping list', function () {
+
+  describe("List is a class", function () {
+    it("should be function", function () {
+      expect(List).to.be.a("function");
+    })
+  })
+  describe("has a property named 'items'", function () {
+    let shoppingList = new List();
+    it("should be function", function () {
+      expect(shoppingList).to.have.property("items");
+    })
+    it('the items property should be an array', function () {
+      expect(shoppingList.items).to.be.a("array");
+    })
+  })
+  describe("has a method named addItem", function () {
+    let shoppingList = new List();
+    it("should be a function", function () {
+      expect(shoppingList.addItem).to.be.a("function");
+    })
+    it("should add an Item object to the items array", function () {
+      let steak = new Item('steak', 't-bone steak');
+      shoppingList.addItem(steak);
+      expect(shoppingList.items[0]).to.deep.equal(steak);
+    })
+    it("should return an error with anything other than an Item object", function () {
+      expect(shoppingList.addItem('cookies')).to.throw();
+    })
+  })
+  describe("has a method named removeItem", function () {
+    let shoppingList = new List();
+    it("should be a function", function () {
+      expect(shoppingList.removeItem).to.be.a("function");
+    })
+    it("should remove an Item object from the items array", function () {
+      let steak = new Item('steak', 't-bone steak');
+      let cookies = new Item('cookies', 'chocolate chip');
+      shoppingList.addItem(steak);
+      shoppingList.addItem(cookies);
+      shoppingList.removeItem(steak);
+      expect(shoppingList.items[1]).to.equal(undefined);
+    })
+    it("should return an error if item does not exist in the list", function () {
+      let steak = new Item('steak', 't-bone steak');
+      let cookies = new Item('cookies', 'chocolate chip');
+      shoppingList.addItem(cookies);
+      expect(shoppingList.removeItem(steak)).to.throw();
+    })
+    it("should remove the last item in the list if there is no input", function () {
+      let steak = new Item('steak', 't-bone steak');
+      let cookies = new Item('cookies', 'chocolate chip');
+      shoppingList.addItem(cookies);
+      shoppingList.addItem(steak);
+      shoppingList.removeItem();
+      expect(shoppingList.items[1]).to.equal(undefined);
+    })
+    it("should return an error with anything other than an Item object", function () {
+      expect(shoppingList.removeItem('cookies')).to.throw();
+    })
+  })
+
+  describe("has a method named render", function () {
+    let shoppingList = new List();
+    it("should be a function", function () {
+      expect(shoppingList.render).to.be.a("function");
+    })
+
+    it("should return a string", function () {
+      expect(shoppingList.render()).to.be.a("string");
+    })
+
+    it("should start with an <li tag", function () {
+      expect(shoppingList.render().slice(0, 3)).to.equal('<li');
+
+    })
+    it("should end with an </li> tag", function () {
+      expect(shoppingList.render().slice(-5)).to.equal('</li>');
     })
   })
 })
